3c5ef0325c42b02387891f4ca8d29eac649d0d35
guowei <guowei@localhost>
2021-08-11 01:14:51 +0800

    citest

--- WordUtil/src/main/java/com/example/WordUtilApplication.java
+++ WordUtil/src/main/java/com/example/WordUtilApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WordUtilApplication {
 	
 	public static void main(String[] args) {
-		//CIdockerHubn
+		//CItest 
 		SpringApplication.run(WordUtilApplication.class, args);
 		
 	}
