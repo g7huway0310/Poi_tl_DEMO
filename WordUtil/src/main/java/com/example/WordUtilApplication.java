996aa53bf1fa5163c6ef50a40ef97dee9d848742
guowei <guowei@localhost>
2021-08-11 01:03:47 +0800

    citest

--- WordUtil/src/main/java/com/example/WordUtilApplication.java
+++ WordUtil/src/main/java/com/example/WordUtilApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WordUtilApplication {
 	
 	public static void main(String[] args) {
-		//CItest
+		//CItestnn
 		SpringApplication.run(WordUtilApplication.class, args);
 		
 	}
