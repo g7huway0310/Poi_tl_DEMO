acc847f9ce4d271e61c3dfc437ceb9da991e0454
guowei <guowei@localhost>
2021-08-11 01:11:47 +0800

    ci

--- WordUtil/src/main/java/com/example/WordUtilApplication.java
+++ WordUtil/src/main/java/com/example/WordUtilApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WordUtilApplication {
 	
 	public static void main(String[] args) {
-		//CIdockerHub
+		//CIdockerHubn
 		SpringApplication.run(WordUtilApplication.class, args);
 		
 	}
