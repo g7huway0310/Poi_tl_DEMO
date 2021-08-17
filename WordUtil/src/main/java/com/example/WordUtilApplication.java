179e3d7902c62bd2662d12c9b4a42846e6b7d564
guowei <guowei@localhost>
2021-08-17 21:25:27 +0800

    ok

--- WordUtil/src/main/java/com/example/WordUtilApplication.java
+++ WordUtil/src/main/java/com/example/WordUtilApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WordUtilApplication {
 	
 	public static void main(String[] args) {
-		//Ci
+		//Cil
 		SpringApplication.run(WordUtilApplication.class, args);
 		
 	}
